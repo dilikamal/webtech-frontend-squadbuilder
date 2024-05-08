d27e831b54242379a14d9ed82311b5dc777be5e6
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-05-08 16:28:33 +0200

    first commit

---
+++ src/views/TeamsOpenApiView.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TeamsOptionsApiList from '@/components/TeamsOptionsApiList.vue'
+
+export default defineComponent({
+  name: 'TeamsOptionsApiView',
+  components: { TeamsOptionsApiList }
+})
+</script>
+
+<template>
+  <main>
+    <TeamsOptionsApiList title="Teams" />
+  </main>
+</template>
+
+<style scoped></style>
