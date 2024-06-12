93c39d7b9b01b53a6db8a57364c5b70d537a3330
dilikamal <146081176+dilikamal@users.noreply.github.com>
2024-06-12 16:50:29 +0200

    PCÜ

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import TeamsList from '@/components/TeamsList.vue';
+
+
+</script>
+
+<template>
+  <main>
+      <h1>This is a teams page</h1>
+
+      <RouterLink to="/teams-oa">Team erstellen</RouterLink>
+    </main>
+</template>
